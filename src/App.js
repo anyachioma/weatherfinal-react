2e90c3e7cf96eb4b2c6ba5a0581ddf4244e24ec5
anyachioma <123375331+anyachioma@users.noreply.github.com>
2023-09-18 19:09:59 +0100

    Updated CSS and JS files

--- src/App.js
+++ src/App.js
@@ -7,32 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Lagos" />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https//www.linkedin.com/in/mfonobong-nnamseh-46529272"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            Anya Chioma{" "}
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/anyachioma/weatherfinal-react.git"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on Github
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://spectacular-gumdrop-37e5e3.netlify.app/"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
+        
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -63,6 +63,32 @@ export default function Weather(props) {
         </form>
         <WeatherInfo data={weatherData} />
         <WeatherForecast coordinates={weatherData.coordinates} />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https//www.linkedin.com/in/mfonobong-nnamseh-46529272"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            Anya Chioma{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/anyachioma/weatherfinal-react.git"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://spectacular-gumdrop-37e5e3.netlify.app/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     );
   } else {
